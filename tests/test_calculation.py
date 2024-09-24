8c1a9595d12bb4ee7c9ef76d25b3c6d6a32b080a
Jin Cheng <jincheng411@gmail.com>
2024-09-24 23:28:55 +0300

    add more test case for calculation.perform

--- tests/test_calculation.py
+++ tests/test_calculation.py
@@ -1,8 +1,28 @@
 '''Test cases for Calculation class'''
+from decimal import Decimal
+import pytest
 from calculator.operations import Operations
 from calculator.calculation import Calculation
 
-def test_calculation_operations():
+@pytest.mark.parametrize("operand_a, operand_b, operation, expected", [
+    (Decimal('3'), Decimal('4'), Operations.add, Decimal('7')),
+    (Decimal('3.4'), Decimal('4.2'), Operations.add, Decimal('7.6')),
+    (Decimal('0'), Decimal('4'), Operations.add, Decimal('4')),
+    (Decimal('3'), Decimal('0'), Operations.add, Decimal('3')),
+    (Decimal('4'), Decimal('3'), Operations.subtract, Decimal('1')),
+    (Decimal('3'), Decimal('3'), Operations.subtract, Decimal('0')),
+    (Decimal('2'), Decimal('4'), Operations.subtract, Decimal('-2')),
+    (Decimal('7.2'), Decimal('4.1'), Operations.subtract, Decimal('3.1')),
+    (Decimal('3'), Decimal('4'), Operations.multiply, Decimal('12')),
+    (Decimal('3.4'), Decimal('4.8'), Operations.multiply, Decimal('16.32')),
+    (Decimal('0'), Decimal('4'), Operations.multiply, Decimal('0')),
+    (Decimal('3'), Decimal('-4'), Operations.multiply, Decimal('-12')),
+    (Decimal('12'), Decimal('4'), Operations.division, Decimal('3')),
+    (Decimal('0'), Decimal('4'), Operations.division, Decimal('0')),
+    (Decimal('3.8'), Decimal('2'), Operations.division, Decimal('1.9')),
+
+])
+def test_calculation_operations(operand_a, operand_b, operation, expected):
     '''Test perform method'''
-    cal = Calculation(1, 2, Operations.add)
-    assert cal.perform() == 3
+    cal = Calculation(operand_a, operand_b, operation)
+    assert cal.perform() == expected
